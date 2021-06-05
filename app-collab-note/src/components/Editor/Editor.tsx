085dd754aef4f9f6183c30afda5411066c573875
Aksh-Konda <aksh.konda@gmail.com>
2021-06-05 14:29:07 +0530

    initial commit

---
+++ app-collab-note/src/components/Editor/Editor.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const container = styled.div`
+    
+`;
+
+export default function Editor() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
