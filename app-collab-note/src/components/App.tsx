79c3830c3b3db277ff02692660a317b2ed00ba2b
Aksh-Konda <aksh.konda@gmail.com>
2021-06-06 19:38:13 +0530

    WebApp: Added Home component and react-router-dom

--- app-collab-note/src/components/App.tsx
+++ app-collab-note/src/components/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './App.css';
+import Editor from './Editor/Editor';
+
+import Home from './Home'
+
+
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/collab' exact component={Editor} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- app-collab-note/src/components/Editor/Editor.tsx
+++ app-collab-note/src/components/Editor/Editor.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import styled from 'styled-components';
 
 const container = styled.div`
-    
+    text-align: center
 `;
 
 export default function Editor() {
     return (
         <div>
-            
+            <h1>Editor goes here</h1>
         </div>
     )
 }
