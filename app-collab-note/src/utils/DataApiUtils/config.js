9ddd86f40093ad0359b5b3b9a73cc1b392198ef5
Aksh-Konda <aksh.konda@gmail.com>
2021-06-09 18:47:37 +0530

    cleanup

--- app-collab-note/src/utils/DataApiUtils/config.js
+++ app-collab-note/src/utils/DataApiUtils/config.js
@@ -1,5 +1,5 @@
 const config_ = {
-    startPort: 41184 || parseInt(process.env.REACT_APP_CLIPPER_PORT_START)
+    startPort: parseInt(process.env.REACT_APP_CLIPPER_PORT_START)
 }
 
 export const config = () => {
