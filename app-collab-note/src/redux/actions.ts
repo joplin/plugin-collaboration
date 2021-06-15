fc938462bffe82c6313bec9014c3afcde879fb03
Aksh-Konda <aksh.konda@gmail.com>
2021-06-15 23:04:51 +0530

    WebApp: resetting indention to match all other files and changed target in tsconfig to es2015

--- app-collab-note/src/redux/actions.ts
+++ app-collab-note/src/redux/actions.ts
@@ -10,142 +10,142 @@ export const SET_HOST_JOINED = 'SET_HOST_JOINED';
 export const RESET_STATE = 'RESET_STATE';
 
 export interface UserConfig {
-    username: string,
-    isHost: boolean,
-    noteId?: string,
-    roomId: string,
-    token?: string
+  username: string,
+  isHost: boolean,
+  noteId?: string,
+  roomId: string,
+  token?: string
 }
 
 export enum MessageType {
-    LOADING = 'LOADING',
-    ERROR = 'ERROR',
-    SUCCESS = 'SUCCESS',
+  LOADING = 'LOADING',
+  ERROR = 'ERROR',
+  SUCCESS = 'SUCCESS',
 }
 
 export interface ApiStatus {
-    messageType: MessageType,
-    status?: string,
-    message: string,
+  messageType: MessageType,
+  status?: string,
+  message: string,
 }
 
 export interface Action {
-    type: string,
-    payload: any
+  type: string,
+  payload: any
 }
 
 function setApiStatus(apiStatus: ApiStatus | null): Action {
-    return {
-        type: SET_API_STATUS,
-        payload: {
-            apiStatus
-        }
+  return {
+    type: SET_API_STATUS,
+    payload: {
+      apiStatus
     }
+  }
 }
 
 function setUserDetails(userConfig: UserConfig): Action {
-    return {
-        type: CONFIG_USER,
-        payload: userConfig
-    }
+  return {
+    type: CONFIG_USER,
+    payload: userConfig
+  }
 }
 
 function setNoteDetails(note: Note): Action {
-    return {
-        type: SET_NOTE,
-        payload: {
-            note
-        }
+  return {
+    type: SET_NOTE,
+    payload: {
+      note
     }
+  }
 }
 
 function setHostJoined(hostJoined: boolean): Action {
-    return {
-        type: SET_HOST_JOINED,
-        payload: {
-            hostJoined
-        }
+  return {
+    type: SET_HOST_JOINED,
+    payload: {
+      hostJoined
     }
+  }
 }
 
 function resetState() {
-    return {
-        type: RESET_STATE,
-        payload: null
-    }
+  return {
+    type: RESET_STATE,
+    payload: null
+  }
 }
 
 function configureUserDetails(userConfig: UserConfig) {
-    return (dispatch: any) => {
-        dispatch(setApiStatus(null));
+  return (dispatch: any) => {
+    dispatch(setApiStatus(null));
 
-        if (userConfig.isHost) {
-            const { token, noteId } = userConfig;
+    if (userConfig.isHost) {
+      const { token, noteId } = userConfig;
 
-            dispatch(setApiStatus({
-                messageType: MessageType.LOADING,
-                status: SEARCHING,
-                message: 'Searching for Joplin Data API',
-            }));
+      dispatch(setApiStatus({
+        messageType: MessageType.LOADING,
+        status: SEARCHING,
+        message: 'Searching for Joplin Data API',
+      }));
 
-            bridge().init(token)
-                .then(() => {
-                    dispatch(setApiStatus({
-                        messageType: MessageType.SUCCESS,
-                        status: FOUND,
-                        message: 'Found Joplin Data API',
-                    }));
+      bridge().init(token)
+        .then(() => {
+          dispatch(setApiStatus({
+            messageType: MessageType.SUCCESS,
+            status: FOUND,
+            message: 'Found Joplin Data API',
+          }));
 
-                    return bridge().getNote(noteId, ['id', 'title', 'body'])
-                        .catch(err => {
-                            throw new Error('Note not found!');
-                        });
-                })
-                .then(note => {
-                    dispatch(setApiStatus({
-                        messageType: MessageType.SUCCESS,
-                        status: FOUND,
-                        message: 'Found Note! Happy Collaboration!!',
-                    }));
+          return bridge().getNote(noteId, ['id', 'title', 'body'])
+            .catch(err => {
+              throw new Error('Note not found!');
+            });
+        })
+        .then(note => {
+          dispatch(setApiStatus({
+            messageType: MessageType.SUCCESS,
+            status: FOUND,
+            message: 'Found Note! Happy Collaboration!!',
+          }));
 
-                    dispatch(setNoteDetails(note));
-                    dispatch(setUserDetails(userConfig));
-                    dispatch(push('/collab'));
-                })
-                .catch(err => {
-                    dispatch(setApiStatus({
-                        messageType: MessageType.ERROR,
-                        message: err.message,
-                    }));
-                });
-        }
-        else {
-            dispatch(setUserDetails(userConfig));
-            dispatch(push('/collab'));
-        }
-    };
+          dispatch(setNoteDetails(note));
+          dispatch(setUserDetails(userConfig));
+          dispatch(push('/collab'));
+        })
+        .catch(err => {
+          dispatch(setApiStatus({
+            messageType: MessageType.ERROR,
+            message: err.message,
+          }));
+        });
+    }
+    else {
+      dispatch(setUserDetails(userConfig));
+      dispatch(push('/collab'));
+    }
+  };
 }
 
-function handleHostJoined(hostJoined: boolean) {
-    return (dispatch: any, getState: any) => {
-        dispatch(setHostJoined(hostJoined));
+function handleHostStatusChange(hostJoined: boolean) {
+  return (dispatch: any, getState: any) => {
+    dispatch(setHostJoined(hostJoined));
 
-        const state = getState();
-        if (!state.isHost) {
-            if (hostJoined) {
-                dispatch(setApiStatus({
-                    messageType: MessageType.SUCCESS,
-                    message: 'Host online!',
-                }));
-            }
-            else {
-                dispatch(setApiStatus({
-                    messageType: MessageType.LOADING,
-                    message: 'Host offline, Waiting for the host...',
-                }));
-            }
-        }
-    };
+    const state = getState();
+    if (!state.isHost) {
+      if (hostJoined) {
+        dispatch(setApiStatus({
+          messageType: MessageType.SUCCESS,
+          message: 'Host online!',
+        }));
+      }
+      else {
+        dispatch(setApiStatus({
+          messageType: MessageType.LOADING,
+          message: 'Host offline, Waiting for the host...',
+        }));
+      }
+    }
+  };
 }
 
-export { setUserDetails, setApiStatus, setNoteDetails, resetState, configureUserDetails, handleHostJoined }
+export { setUserDetails, setApiStatus, setNoteDetails, resetState, configureUserDetails, handleHostStatusChange }
--- app-collab-note/src/redux/reducer.ts
+++ app-collab-note/src/redux/reducer.ts
@@ -1,54 +1,63 @@
 import { connectRouter } from "connected-react-router";
 import { combineReducers } from "redux";
-import { Action, CONFIG_USER, RESET_STATE, SET_API_STATUS, SET_HOST_JOINED, SET_NOTE, UserConfig } from "./actions";
 import { History } from 'history';
+
+import {
+  Action,
+  CONFIG_USER,
+  RESET_STATE,
+  SET_API_STATUS,
+  SET_HOST_JOINED,
+  SET_NOTE,
+  UserConfig,
+} from "./actions";
 import { initialState } from "./configStore";
 
 function reducer(state = initialState.app, action: Action) {
 
-    switch(action.type) {
-        case CONFIG_USER:{
-            const config = action.payload as UserConfig;
-            const newState = { ...state}
-            if(config.isHost) {
-                const { isHost, username, token, noteId } = config;
-                Object.assign(newState, { isHost, username, authToken: token, noteId, roomId: noteId });
-            }
-            else {
-                const { isHost, username, roomId } = config;
-                Object.assign(newState, { isHost, username, roomId })
-            }
-            return newState;
-        }
-        case SET_NOTE: {
-            const { note } = action.payload;
-            if(!!note) {
-                const newState = { ...state, note };
-                return newState;
-            }
-            break;
-        }
-        case SET_API_STATUS: {
-            const { apiStatus } = action.payload;
-            const newState = { ...state, apiStatus };
-            return newState;
-        }
-        case SET_HOST_JOINED: {
-            const { hostJoined } = action.payload;
-            const newState = { ...state, hostJoined };
-            return newState;
-        }
-        case RESET_STATE: {
-            return { ...initialState.app };
-        }
+  switch (action.type) {
+    case CONFIG_USER: {
+      const config = action.payload as UserConfig;
+      const newState = { ...state }
+      if (config.isHost) {
+        const { isHost, username, token, noteId } = config;
+        Object.assign(newState, { isHost, username, authToken: token, noteId, roomId: noteId });
+      }
+      else {
+        const { isHost, username, roomId } = config;
+        Object.assign(newState, { isHost, username, roomId })
+      }
+      return newState;
     }
+    case SET_NOTE: {
+      const { note } = action.payload;
+      if (!!note) {
+        const newState = { ...state, note };
+        return newState;
+      }
+      break;
+    }
+    case SET_API_STATUS: {
+      const { apiStatus } = action.payload;
+      const newState = { ...state, apiStatus };
+      return newState;
+    }
+    case SET_HOST_JOINED: {
+      const { hostJoined } = action.payload;
+      const newState = { ...state, hostJoined };
+      return newState;
+    }
+    case RESET_STATE: {
+      return { ...initialState.app };
+    }
+  }
 
-    return state;
+  return state;
 }
 
 const createRootReducer = (history: History) => combineReducers({
-    router: connectRouter(history),
-    app: reducer,   
+  router: connectRouter(history),
+  app: reducer,
 });
 
 export default createRootReducer;
