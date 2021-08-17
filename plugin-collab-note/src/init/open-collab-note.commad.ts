cf8cc3b41bd07ba598ce4746e1da3a6e9f931d53
Aksh-Konda <aksh.konda@gmail.com>
2021-08-18 01:09:44 +0530

    Plugin: Updated README and cleanup

--- plugin-collab-note/src/init/open-collab-note.commad.ts
+++ plugin-collab-note/src/init/open-collab-note.commad.ts
@@ -8,7 +8,7 @@ import {
 const opener = require('opener');
 
 function isValidUrl(url: string): boolean {
-  const urlRegex = /^((http(s)?):\/\/)([wW]{3}\.)?[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$/g;
+  const urlRegex = /^((http(s)?):\/\/)([wW]{3}\.)?[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?(\/)?$/g;
   const result = url.match(urlRegex);
   return result !== null || url.startsWith('http://localhost:');
 };
@@ -19,17 +19,29 @@ export async function registerOpenCollabNoteCommand() {
     label: 'Open note in Collab note web app',
     iconName: PLUGIN_ICON,
     execute: async () => {
-      const webAppURL: string = await joplin.settings.value(COLLAB_WEB_APP_URL_ID);
-      const token: string = await joplin.settings.globalValue('api.token');
-      const note: { id: string } = await joplin.workspace.selectedNote();
-      const validUrl = isValidUrl(webAppURL);
-      console.log('Collboration plugin:',{ WebAppURL: webAppURL, token, note, isValidUrl: validUrl });
+      let webAppURL: string = await joplin.settings.value(COLLAB_WEB_APP_URL_ID);
 
-      if(validUrl) {
-        opener(`${webAppURL}/${note.id}/${token}`);
+      if(!webAppURL) {
+        await joplin.views.dialogs.showMessageBox(`
+        Error: URL to the Collab Note web app is required!
+        Please provide the URL in 'Tools>Options>Collab Note Plugin' section.
+        `);
+        return;
+      }
+
+      if(!webAppURL.endsWith('/')) {
+        webAppURL = webAppURL + '/';
+      }
+
+      if(isValidUrl(webAppURL)) {
+        const token: string = await joplin.settings.globalValue('api.token');
+        const note: { id: string } = await joplin.workspace.selectedNote();
+        console.log('Collboration plugin:',{ WebAppURL: webAppURL, token, note });
+        opener(`${webAppURL}${note.id}/${token}`);
       }
       else {
-        console.error('Plugin-Collaboration: not a valid URL!');
+        await joplin.views.dialogs.showMessageBox(`Error: Invalid URL! - ${webAppURL}`);
+        return;
       }
     }
   });
